19eed33d15f540a8d890e234abf2893a9fb181fb
Mauricio Arce Torrez <mauricio.arce.mat@gmail.com>
2024-04-16 14:57:55 -0400

    Update banner styles and general fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      colors: { gray: "#626c75" },
+      colors: { gray: '#626c75' },
       backgroundImage: {
-        'desktop-about-section': "url('/src/assets/images/bg-about-us-desktop.svg')",
-        'mobile-about-section': "url('/src/assets/images/bg-about-us-mobile.svg')",
-      }
+        'hero-section': "url('/src/assets/images/black-lines.svg')",
+        'desktop-about-section':
+          "url('/src/assets/images/bg-about-us-desktop.svg')",
+        'mobile-about-section':
+          "url('/src/assets/images/bg-about-us-mobile.svg')",
+      },
+      minHeight: {
+        800: '800px',
+      },
     },
   },
   plugins: [],
