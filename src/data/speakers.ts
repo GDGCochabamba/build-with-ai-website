1f329a16759a95ef9e4acdc4c150e971d071c3f5
Angel1104 <flores1102angel266@gmail.com>
2024-05-02 23:53:55 -0400

    Add sponsors

---
+++ src/data/speakers.ts
@@ -0,0 +1,142 @@
+import CamilaVia from "../assets/images/speakers/camila-via.png";
+import FlaviaDavila from "../assets/images/speakers/flavia-davila.png";
+import NatalyGemio from "../assets/images/speakers/nataly-gemio.png";
+import GabrielaTorrico from "../assets/images/speakers/gabriela-torrico.png";
+import AdrianaPaz from "../assets/images/speakers/adriana-paz.png";
+import JacquelineMejia from "../assets/images/speakers/jacqueline-mejia.png";
+import NataliaAlfaro from "../assets/images/speakers/natalia-alfaro.png";
+import MariaGarcia from "../assets/images/speakers/maria-garcia.png";
+import logoWeb from "../assets/images/social/web.svg";
+import logoLinkedin from "../assets/images/social/linkedin.svg";
+import logoFacebook from "../assets/images/social/facebook.svg";
+import logoInstagram from "../assets/images/social/instagram.svg";
+import logoTwitter from "../assets/images/social/twitter.svg";
+import logoGithub from "../assets/images/social/github.svg";
+import logoYoutube from "../assets/images/social/youtube.svg";
+
+export const speakers = [
+  {
+    img: CamilaVia,
+    name: "Camila Flor Via",
+    bio: "Fundadora de NotionIS, desarrolladora de Software con especialización en el desarrollo de software utilizando JavaScript para el frontend y backend.",
+    social: [
+      {
+        social_img: logoFacebook,
+        src: "https://www.facebook.com/Agarratecatalinabol",
+      },
+      {
+        social_img: logoInstagram,
+        src: "https://www.instagram.com/agarratecatalinabol?igsh=ZmU1ajI4MXFhOW0z",
+      },
+    ],
+  },
+  {
+    img: FlaviaDavila,
+    name: "Flavia Maylin Davila Perez",
+    bio: "Ing. Industrial",
+    social: [
+      {
+        social_img: logoInstagram,
+        src: "https://www.instagram.com/justme.fla/?hl=es",
+      },
+    ],
+  },
+  {
+    img: NatalyGemio,
+    name: "Nataly Alejandra Gemio Morales",
+    bio: "Estudiante de informatica en la Universidad Mayor De San Andres.",
+    social: [
+      {
+        social_img: logoFacebook,
+        src: "https://www.facebook.com/nataly.gemio/",
+      },
+      {
+        social_img: logoInstagram,
+        src: "https://www.instagram.com/tugfanat/",
+      },
+      {
+        social_img: logoLinkedin,
+        src: "https://www.linkedin.com/in/nataly-gemio-32602a216/",
+      },
+    ],
+  },
+  {
+    img: GabrielaTorrico,
+    name: "Gabriela Nathaly Torrico",
+    bio: "Agente de soporte técnico IT y Docente de Sistemas Informáticos.",
+    social: [
+      {
+        social_img: logoLinkedin,
+        src: "www.linkedin.com/in/gabriela-torrico-33925416a",
+      },
+    ],
+  },
+  {
+    img: AdrianaPaz,
+    name: "Adriana Eileen Paz Rivas",
+    bio: "Ing. Electromecánica y Profesora de violín.",
+    social: [
+      {
+        social_img: logoInstagram,
+        src: "https://www.instagram.com/adri_gominola/"
+      },
+      {
+        social_img: logoLinkedin,
+        src: "https://www.linkedin.com/in/adriana-eileen-paz-rivas/",
+      }
+    ]
+  },
+  {
+    img: JacquelineMejia,
+    name: "Jacqueline Susan Mejía",
+    bio: "Ingeniera de Sistemas / Data Analyst.",
+    social: [
+      {
+        social_img: logoFacebook,
+        src: "https://www.instagram.com/adri_gominola/"
+      },
+      {
+        social_img: logoInstagram,
+        src: "https://www.instagram.com/susy.jam/"
+      },
+      {
+        social_img: logoLinkedin,
+        src: "https://www.linkedin.com/in/susyjam/"
+      },
+      {
+        social_img: logoTwitter,
+        src: "https://twitter.com/susyjam",
+      }
+    ]
+  },
+  {
+    img: NataliaAlfaro,
+    name: "Natalia Lorena Alfaro Fernández",
+    bio: "Assuresoft - Senior Software Developer",
+    social: [
+      {
+        social_img: logoLinkedin,
+        src: "https://www.linkedin.com/in/natalia-alfaro-dev?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+      }
+    ]
+  },
+  {
+    img: MariaGarcia,
+    name: "Maria Griselda Garcia Flores",
+    bio: "Jalasoft / Software engineer",
+    social: [
+      {
+        social_img: logoFacebook,
+        src: "https://www.facebook.com/combigriss"
+      },
+      {
+        social_img: logoInstagram,
+        src: "https://www.instagram.com/combigriss/"
+      },
+      {
+        social_img: logoLinkedin,
+        src: "https://www.linkedin.com/in/combigriss/"
+      },
+    ]
+  },
+];
