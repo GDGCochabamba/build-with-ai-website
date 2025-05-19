207bf86fc7b8c739fcc0dea6fd798fee41876b41
Angel1104 <flores1102angel266@gmail.com>
2025-05-19 19:56:52 -0400

    feat: add mentors section with detailed profiles and social links

---
+++ src/data/mentors-2025.ts
@@ -0,0 +1,126 @@
+import AdrianaCalvo from "../assets/images/mentors/Adriana Calvo Bueno.jpeg";
+import CarlaFlorida from '../assets/images/mentors/_Y3A1560-Editar - CARLA MARCELA FLORIDA ROMAN.png';
+import AlisonOrellana from '../assets/images/mentors/20231014_013009 (1) - Alison Orellana Rios.jpg';
+import HnasySchmitt from '../assets/images/mentors/40385444_10211841558970553_6288262099109937152_n - Hansy Schmitt.jpeg';
+import SergioNunez from '../assets/images/mentors/1708298193097 - Sergio Mauricio Nunez.jpg';
+import AlejandroNunez from '../assets/images/mentors/AlejandroNunezArroyo - Alejandro Nunez Arroyo.jpg';
+import Ceci from '../assets/images/mentors/CGP2021 - Ceci Gzz.jpg';
+import Dante from '../assets/images/mentors/dante_foto (2) - Dante Rivadeneyra.jpg';
+import AnaEliazara from '../assets/images/mentors/Foto_Eliazara Velasco - Ana Eliazara Velasco Flores.jpeg';
+import StephanieSaavedra from '../assets/images/mentors/Foto-StephanieSaavedraAyarde - Stephanie Saavedra.jpg';
+import MartinArispe from '../assets/images/mentors/IMG_20201212_221506 - Martin Arispe Riveros.jpg';
+import AnaFernandez from '../assets/images/mentors/IMG_20240404_190331 - Ana Fernandez.jpg';
+import SergioFernandez from '../assets/images/mentors/IMG-20220727-WA0010 - Sergio Rodrigo Fernandez Testa.jpg';
+import AraceliArinez from '../assets/images/mentors/IMG-20240410-WA0099(1) - Araceli Arinez.jpg';
+import YuryCamacho from '../assets/images/mentors/inbound3942002585472890971 - Yury Camacho.jpg';
+import RobotsArtifical from '../assets/images/mentors/logo2 - Robots Artificiales.png';
+import HerlandSaanz from '../assets/images/mentors/Photo01 - Herland Saanz Scheer.jpg';
+import FernandoSilva from '../assets/images/mentors/pp - Fernando Silva.png';
+import PabloDaniel from '../assets/images/mentors/profile - Pablo Daniel Noya Quiroz.jpg';
+import RemmyBernardo from '../assets/images/mentors/remmy - Remmy Bernardo Calamani Quispe.jpeg';
+import RonaldChipana from '../assets/images/mentors/RON - Ronald Chipana Wariste.png';
+import SergioAlejandro from '../assets/images/mentors/Screenshot_20240411_220327_Photos - Sergio Alejandro Paucara Saca.jpg';
+import Irvin from '../assets/images/mentors/irvin.jpg'
+import Melvy from '../assets/images/mentors/Melvy Ancieta.jpg';
+
+import logoWeb from "../assets/images/social/web.svg";
+import logoLinkedin from "../assets/images/social/linkedin.svg";
+import logoFacebook from "../assets/images/social/facebook.svg";
+import logoInstagram from "../assets/images/social/instagram.svg";
+import logoTwitter from "../assets/images/social/twitter.svg";
+import logoGithub from "../assets/images/social/github.svg";
+import logoYoutube from "../assets/images/social/youtube.svg";
+
+export const mentors = [
+  {
+    img: AdrianaCalvo,
+    name: "Adriana Calvo Bueno",
+    bio: "Ing. de Software en Jalasoft, ganadora del programa Seeds For The Future de Huawei, ganadora del Hackathon más grande del país (HackBo), reconocida por ser uno de los jóvenes más sobresalientes de su región por los premios Ten Outstanding Young People en liderazgo y logros académicos, TEDx Speaker, segundo lugar en la competencia internacional Scheneider Go Green, parte del primer equipo boliviano ganador de la medalla de oro en la competencia científica internacional iGEM, juez de competencias de robótica y del Hackathon Nasa Space App Challenge, amante de la ciencia y creadora de contenido.",
+    social: [
+      {
+        social_img: logoInstagram,
+        src: "https://www.instagram.com/adri.js_",
+      },
+    ],
+  },
+
+  {
+    img: AlejandroNunez,
+    name: "Alejandro Núñez Arroyo",
+    bio: "Alejandro Núñez Arroyo es Ingeniero Mecatrónico con experiencia en la ejecución de proyectos que hacen uso de la inteligencia artificial. Actualmente, se desempeña como Investigador en Inteligencia Artificial en Fogsphere, una empresa con sede en el Reino Unido. Su experiencia incluye proyectos en áreas como visión por computador, modelos de lenguaje y ciencia de datos. Además, es el organizador del Google Developers Group en Sucre y mentor de ciencias de datos en el programa internacional InTech MOM.",
+    social: [
+      {
+        social_img: logoLinkedin,
+        src: "https://www.linkedin.com/in/alejandronunezarroyo/",
+      },
+      {
+        social_img: logoInstagram,
+        src: "https://www.instagram.com/alenunezarroyo/",
+      },
+      {
+        social_img: logoGithub,
+        src: "https://github.com/AleNunezArroyo",
+      },
+      {
+        social_img: logoTwitter,
+        src: "https://twitter.com/alenunezarroyo",
+      },
+    ],
+  },
+  {
+    img: AlisonOrellana,
+    name: "Alison Orellana Rios",
+    bio: "MSc. Ingeniera Informática, con especialización en Ciencia de Datos, Organizadora de la Comunidad PyLadies Cochabamba, Women Techmaker Ambassador, Instructora de programación y computación en Cisco Networking Academy. Entusiasta del aprendizaje y enseñanza en diferentes áreas.",
+    social: [
+      {
+        social_img: logoLinkedin,
+        src: "https://linktr.ee/AllyOR",
+      },
+    ],
+  },
+  {
+    img: AnaEliazara,
+    name: "Ana Eliazara Velasco Flores",
+    bio: "Eliazara Velasco, ingeniera biomédica boliviana apasionada por la enseñanza e investigación de prótesis y biomateriales.",
+    social: [
+      {
+        social_img: logoLinkedin,
+        src: "https://www.linkedin.com/in/ana-eliazara-velasco-flores-8905341a8",
+      },
+    ],
+  },
+  {
+    img: AnaFernandez,
+    name: "Anahi Raysel Mamani Fernández",
+    bio: "Anahi es estudiante de Ingeniería Mecatrónica en la UMSA. Autora de 3 libros de Ecuaciones diferenciales. Directora Departamental de Mujeres TICs Bolivia. Coordinadora Nacional del All Bolivian Asteroid Search Campaign. Instructora certificada en CISCO Academy, mentora de Inteligencia Artificial y Desarrollo Web en Nos Gusta la Ciencia. Coordinadora Departamental del proyecto de robótica RECIBOT. Además, fué organizadora de el Hackathon TinkuBots UMSA 2023 y el Hackathon Ethereum - 2023. Actualmente es Vicepresidenta de la Asociación Departamental de ajedrez, Tricampeona Sudamericana de Ajedrez, reconocida como la Maestra FIDE más joven en la historia de Bolivia.",
+    social: [
+      {
+        social_img: logoFacebook,
+        src: "https://www.facebook.com/anahi.mamani.393950?mibextid=ZbWKwL",
+      },
+      {
+        social_img: logoInstagram,
+        src: "https://www.instagram.com/anysel_fer?igsh=bXhidzljMWhzeWVs",
+      },
+    ],
+  },
+  {
+    img: AraceliArinez,
+    name: "Andrea Araceli Ariñez Alvarez",
+    bio: "Araceli Ariñez es estudiante de 5to año en la carrera de ingeniería biomédica, durante todo su tiempo de formación académica se ha desenvuelto como líder en áreas dedicadas a la educación, altruismo y desarrollo de programas, proyectos e iniciativas sostenibles para niños y jóvenes. Es remarcable su trabajo en el desarrollo de inicitivas STEAM enfocadas a los ODS 3, 4 y 5 en su entorno académico (proyectos de investigación en biomecánica) y también junto a organizaciones sin fines de lucro (hackatones, summits, etc). Acualmente se encuentra como Pionnner en esta iniciativa, deseosa de poder apoyar como mentora a los proyectos que se relacionen en el área de la salud, ingeniería y mejora de la calidad de vida de las personas de nuestro pais y regiones adyacentes.",
+    social: [
+      {
+        social_img: logoLinkedin,
+        src: "https://www.linkedin.com/in/araceli-ari%C3%B1ez-166b31159/",
+      },
+      {
+        social_img: logoInstagram,
+        src: "https://www.instagram.com/ara__ara__gomen/",
+      },
+      {
+        social_img: logoFacebook,
+        src: "https://www.facebook.com/andrea.aritarinez",
+      },
+    ],
+  }
+];
