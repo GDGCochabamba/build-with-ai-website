d741b22207e63a00adc0bc58007af6667696ec4d
Angel1104 <flores1102angel266@gmail.com>
2025-06-24 18:30:23 -0400

    feat: add agenda conference component and update schedule activities

---
+++ src/data/charlas.js
@@ -0,0 +1,97 @@
+export const cronograma = [
+    {
+      auditorio: "Auditorio A",
+      actividades: [
+        {
+          hora: "10:00 - 10:40",
+          titulo: "AI for Personalized Learning in Bolivia",
+          expositor: "Martin Arispe Riveros"
+        },
+        {
+          hora: "11:00 - 11:40",
+          titulo: "De Estudiante a Startup: El Viaje de Convertir Conocimiento en Innovación",
+          expositor: "Gustavo Gutiérrez Thompson"
+        },
+        {
+          hora: "14:00 - 14:40",
+          titulo: "Tu flujo no necesita IA, necesita morir en paz",
+          expositor: "Jose David Valda Peñaranda"
+        },
+        {
+          hora: "15:00 - 15:40",
+          titulo: "Revolucionando la Gobernanza con IA: Construyendo una Ciudadanía Digital Eficiente",
+          expositor: "Carlos Olivera Terrazas"
+        },
+        {
+          hora: "16:00 - 16:40",
+          titulo: "Hackea tu Proyecto con IA: Potencia tu idea en esta Hackathon con Gemini en Google Cloud",
+          expositor: "Lesly Zerna"
+        },
+        {
+          hora: "17:00 - 17:40",
+          titulo: "DeepTech y el Futuro de la Innovación",
+          expositor: "Fernando Silva"
+        }
+      ]
+    },
+    {
+      auditorio: "Auditorio B",
+      actividades: [
+        {
+          hora: "10:00 - 10:40",
+          titulo: "El Auge de la IA en RRHH: El Caso de Éxito de Trabajito en LATAM",
+          expositor: "Gonzalo Delgadillo Bejarano"
+        },
+        {
+          hora: "11:00 - 11:40",
+          titulo: "Integrando IA en tu flujo de desarrollo: herramientas y buenas prácticas",
+          expositor: "Rodrigo Torrico Del Castillo"
+        },
+        {
+          hora: "14:00 - 14:40",
+          titulo: "Creando prototipos ágiles de AI con Agent Development Kit",
+          expositor: "Nathaly Alarcón Torrico"
+        },
+        {
+          hora: "15:00 - 15:40",
+          titulo: "Resumir para Entender: Aprende Cualquier Tema Técnico con IA",
+          expositor: "Marian Villa"
+        },
+        {
+          hora: "16:00 - 16:40",
+          titulo: "Supercharge Your AI: A Hands-On Workshop with TPUs and GPUs on Google Cloud",
+          expositor: "Camilla Martins"
+        },
+        {
+          hora: "17:00 - 17:40",
+          titulo: "Integra IA en una Angular app fácilmente con hashbrown",
+          expositor: "Nicolás Molina"
+        }
+      ]
+    },
+    {
+      auditorio: "Auditorio C",
+      actividades: [
+        {
+          hora: "14:00 - 14:40",
+          titulo: "Experiencias en adaptación de dominio para la personalización de modelos de IA",
+          expositor: "Alejandro Núñez Arroyo"
+        },
+        {
+          hora: "15:00 - 15:40",
+          titulo: "Emprender con IA desde Bolivia: Innovar con talento, tecnología y visión en tiempos complejos",
+          expositor: "Marco Antonio Gomez Ortega Callau"
+        },
+        {
+          hora: "16:00 - 16:40",
+          titulo: "Inteligencia Artificial para ordenar el caos: cómo organizar y consultar archivos empresariales con IA",
+          expositor: "Helen Steffani Limachi Loayza"
+        },
+        {
+          hora: "17:00 - 17:40",
+          titulo: "Más Allá del Chat: IA como motor de Inclusión y Productividad en Bolivia",
+          expositor: "Sergio Rodrigo Cárdenas Rivera"
+        }
+      ]
+    }
+  ];
